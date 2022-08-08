46dfd29f44fc2453229b1f7011ce81baa2b6417e
Valentin Bragaru <valentinb@readcast.net>
2022-08-08 14:48:05 +0300

    change miliseconds in seconds

--- api-app/routes/time.js
+++ api-app/routes/time.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET epoch time. */
 router.get('/', function (req, res, next) {
-  res.json({ epoch: new Date().getTime() });
+  res.json({ epoch: Math.floor(new Date().getTime() / 1000) });
 });
 
 module.exports = router;
