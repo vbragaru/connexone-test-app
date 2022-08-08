a0ac71f0da6f554f800228d754b8222dff7a6392
Valentin Bragaru <valentinb@readcast.net>
2022-08-08 11:55:56 +0300

    add morgan logger

--- api-app/app.js
+++ api-app/app.js
@@ -2,13 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 var prometheus = require('./middlewares/prometheus');
 var time = require('./routes/time');
 
 var app = express();
-
 app.use(prometheus);
+
+app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
