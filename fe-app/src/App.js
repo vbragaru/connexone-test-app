f67213ab0895051fd776f1fa40a480370688cb6d
Valentin Bragaru <valentinb@readcast.net>
2022-08-08 15:50:12 +0300

    add time

---
+++ fe-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Timer from './components/time/Time'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Timer></Timer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fe-app/src/components/time/Time.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+
+import axios from 'axios';
+import moment from 'moment';
+
+export default class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            serverTime: 0,
+            clientTime: 0,
+            loading: true
+        };
+
+    }
+
+    async serverTimeTick() {
+        this.setState(state => ({
+            ...state,
+            loading: true
+        }));
+        const serverTime = await this.getServerTime();
+        this.setState(state => ({
+            ...state,
+            serverTime: serverTime,
+            loading: false
+        }));
+    }
+
+    async getServerTime() {
+        let config = {
+            headers: { 'Authorization': 'mysecrettoken' },
+        };
+        const serverTime = await axios.get('http://localhost:3001/api/v1/time', config);
+        return serverTime.data.epoch;
+    }
+
+    clientTimeTick() {
+        const clientTime = Math.floor(new Date().getTime() / 1000);
+        this.setState(state => ({
+            ...state,
+            clientTime: clientTime,
+        }));
+    }
+
+    componentDidMount() {
+        this.serverTimeTick();
+        this.clientTimeTick();
+
+        this.serverInterval = setInterval(() => this.serverTimeTick(), 10000);
+        this.clientInterval = setInterval(() => this.clientTimeTick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.serverInterval);
+        clearInterval(this.clientInterval);
+    }
+
+    render() {
+        return (this.state.loading ? <div>loading...</div> :
+            <div>
+                <p>serverTime: {this.state.serverTime}</p >
+                <p>clientTime: {this.state.clientTime}</p>
+                <p>difference: {moment.utc(moment.duration(this.state.clientTime - this.state.serverTime, 'seconds').asMilliseconds()).format('HH:mm:ss')}</p>
+            </div >
+        );
+    }
+}
